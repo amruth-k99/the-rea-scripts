91551e4114d9d92fc12ec7ef5706316a63c98beb
Amruth Kuntamalla <amruthk99@gmail.com>
2020-07-30 18:44:15 +0530

    completed binary_to_decimal.py

--- GeneralProblems/binary_to_decimal.py
+++ GeneralProblems/binary_to_decimal.py
@@ -6,6 +6,9 @@ def bin_convert(n):
         rem = n % 10
         if rem is 1:
             dec = dec + 2**p
+        if rem is not 1 and rem is not 0:
+            return ("Please enter correct Binary value")
+
         p = p+1
         n = int(n/10)
     return dec
